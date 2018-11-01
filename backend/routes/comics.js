a53357676a1363ac9041133832fe179990f67682
John Briggs <sirbriggs@gmail.com>
2018-11-01 07:53:20 -0500

    oops

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -29,7 +29,7 @@ router.get('/:slug', function(req, res, next) {
 router.get('/:slug/navigation', function(req, res, next) {
   console.log(req.params.slug);
 
-  var currentQuery = 'SELECT date FROM comics WHERE slug ="'+req.params.slug+'";';
+  var currentQuery = 'SELECT date FROM comics WHERE slug ="'+req.params.slug+'"';
   var previousQuery = 'SELECT slug FROM comics WHERE date < ('+currentQuery+') order by date desc limit 1;';
   var nextQuery = 'SELECT slug FROM comics WHERE date > ('+currentQuery+') order by date desc limit 1;';
   var randomQuery = 'SELECT slug FROM comics ORDER BY RAND() WHERE slug <> "'+req.params.slug+'" LIMIT 1;';
