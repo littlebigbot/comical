ef6465351d42a1b110624ccc1a1db78d7dc2cbf7
John Briggs <sirbriggs@gmail.com>
2018-10-31 20:51:13 -0500

    fixed

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ var comics = require('./routes/comics');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, '../static'));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
