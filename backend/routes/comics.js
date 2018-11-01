a62baef7695e71701aa6f44e964713c21e95fe8d
John Briggs <sirbriggs@gmail.com>
2018-10-31 21:40:04 -0500

    fixed

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -14,4 +14,17 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:slug', function(req, res, next) {
+  console.log(req);
+  connection.query('SELECT * from comics', function (error, results, fields) {
+    if(error){
+      res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+      //If there is error, we send the error in the error section with 500 status
+    } else {
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      //If there is no error, all is good and response is 200OK.
+    }
+  });
+});
+
 module.exports = router;
