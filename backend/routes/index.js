c8113f7163c807cd491ee5c3bc538322cf2b011e
John Briggs <sirbriggs@gmail.com>
2018-10-31 20:46:25 -0500

    fixed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/static/index.html'));
