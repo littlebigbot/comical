43f11dedd9b37a4721bdea82607226be48e9bc01
John Briggs <sirbriggs@gmail.com>
2018-11-02 16:16:26 -0500

    test

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/last', function(req, res, next) {
-  connection.query('SELECT slug FROM comics ORDER BY date DESC LIMIT 1;', function (error, results, fields) {
+  connection.query('SELECT * FROM comics ORDER BY date DESC LIMIT 1;', function (error, results, fields) {
     if(error){
       res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
       //If there is error, we send the error in the error section with 500 status
