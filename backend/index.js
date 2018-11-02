fe577da7df0bf6baf7d990da5fb98e7801894369
John Briggs <sirbriggs@gmail.com>
2018-11-02 16:18:32 -0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,12 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.resolve(__dirname, '../static')));
 
 // uncomment after placing your favicon in /public
--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -20,7 +20,7 @@ router.get('/last', function(req, res, next) {
       res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
       //If there is error, we send the error in the error section with 500 status
     } else {
-      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results[0]}));
       //If there is no error, all is good and response is 200OK.
     }
   });
@@ -32,7 +32,7 @@ router.get('/:slug', function(req, res, next) {
       res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
       //If there is error, we send the error in the error section with 500 status
     } else {
-      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results[0]}));
       //If there is no error, all is good and response is 200OK.
     }
   });
