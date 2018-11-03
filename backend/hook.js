b9fbed68b49902b148c168c11672cdbb8e0f7780
John Briggs <sirbriggs@gmail.com>
2018-11-03 11:20:34 -0500

    test

--- backend/hook.js
+++ backend/hook.js
@@ -3,11 +3,14 @@ var gith = require('gith').create( 9001 );
 // Import execFile, to run our bash script
 var execFile = require('child_process').execFile;
 
+console.log('im here!')
+
 gith({
     repo: 'littlebigbot/comical'
 }).on( 'all', function( payload ) {
-    if( payload.branch === 'master' )
+    if( payload.branch === 'default' )
     {
+      console.log('default!')
             // Exec a shell script
             execFile('/hook.sh', function(error, stdout, stderr) {
                     // Log success in some manner
