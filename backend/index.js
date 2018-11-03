bea754c8e65a79761e23ecc21bb699b9a25ece82
John Briggs <sirbriggs@gmail.com>
2018-11-02 21:14:10 -0500

    for now

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,8 @@ var comics = require('./routes/comics');
 
 var app = express();
 
+app.use(cors())
+
 // view engine setup
 app.set('views', path.resolve(__dirname, '../static'));
 app.engine('html', require('ejs').renderFile);
@@ -46,12 +48,12 @@ app.use(function(req, res, next){
 });
 
 // app.use('/', index);
-app.use('/api/v1/comics', cors(), comics);
+app.use('/api/v1/comics', comics);
 
-app.use("*", function(req, resp) {
-  console.log(path.resolve(__dirname + '../../static/index.html'));
-  resp.sendFile(path.resolve(__dirname + '../../static/index.html'));
-});
+// app.use("*", function(req, resp) {
+//   console.log(path.resolve(__dirname + '../../static/index.html'));
+//   resp.sendFile(path.resolve(__dirname + '../../static/index.html'));
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
